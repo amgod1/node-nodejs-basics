7418823888bdedad3494318c19d2cd211ed4e62a
amgod1 <kamillschepuk@gmail.com>
2025-04-25 09:35:30 +0300

    feat: create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,34 @@
-const create = async () => {
-    // Write your code here 
-};
+import { constants } from "node:fs"
+import { access, appendFile } from "node:fs/promises"
 
-await create();
+const isFileExist = async (path) => {
+  let isExist = false
+
+  try {
+    await access(path, constants.F_OK)
+    isExist = true
+  } catch {
+    isExist = false
+  }
+
+  return isExist
+}
+
+const create = async (title, data) => {
+  const path = `./files/${title}`
+
+  try {
+    const isExist = await isFileExist(path)
+
+    if (isExist) throw new Error("FS operation failed")
+
+    appendFile(path, data)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const title = "fresh.txt"
+const data = "I am fresh and young"
+
+await create(title, data)
