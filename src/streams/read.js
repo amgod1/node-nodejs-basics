688d0de490f185d33cf9b818eecdb24c0908151a
kamill <kamillschepuk@gmail.com>
2025-04-28 17:03:55 +0200

    feat: streams tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
-const read = async () => {
-    // Write your code here 
-};
+import { join } from "node:path"
+import { createReadStream } from "node:fs"
 
-await read();
+const read = async (readPath) => {
+  const readStream = createReadStream(readPath)
+
+  return new Promise((resolve, reject) => {
+    readStream.on("data", (chunk) => {
+      process.stdout.write(chunk)
+    })
+
+    readStream.on("end", resolve)
+    readStream.on("error", reject)
+  })
+}
+
+const readPath = join("files", "fileToRead.txt")
+await read(readPath)
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
-const write = async () => {
-    // Write your code here 
-};
+import { join } from "node:path"
+import { createWriteStream } from "node:fs"
 
-await write();
+const write = async (writePath) => {
+  const writeStream = createWriteStream(writePath)
+
+  process.stdin.pipe(writeStream)
+
+  return new Promise((resolve, reject) => {
+    writeStream.on("finish", resolve)
+    writeStream.on("error", reject)
+  })
+}
+
+const writePath = join("files", "fileToWrite.txt")
+await write(writePath)
